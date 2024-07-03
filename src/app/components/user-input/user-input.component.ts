6af94cb303a7dfae8f3406918a2490d02e975e7b
Chema Muñoz <chemunoz78@gmail.com>
2024-07-03 14:22:27 +0200

    feat: output user-input  data

--- src/app/components/user-input/user-input.component.ts
+++ src/app/components/user-input/user-input.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '@interfaces/investment-input.interface';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  calculate = output<InvestmentInput>();
+
+  enteredInitialInvestment: string = '0';
+  enteredAnnualInvestment: string = '0';
+  enteredExpectedReturn: string = '5';
+  enteredDuration: string = '10';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedReturn: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
+  }
+}
